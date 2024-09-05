cd1b3f04fab6ed5e81217feff380ba87deedc73a
Arshad Bava M <103155503+ArshadBava@users.noreply.github.com>
2024-09-05 10:14:05 +0530

    Update dbms ex 3.sql

--- dbms ex 3.sql
+++ dbms ex 3.sql
@@ -1,5 +1,5 @@
-create database universitydb;
-use universitydb;
+create database university_db;
+use university_db;
 
 create table Students (
 student_id int primary key,
@@ -42,7 +42,7 @@ where i.department = 'Banking Technology';
 
 select s.student_id, s.student_name, s.age from Students s
 left join Enrolments e on s.student_id = e.student_id
-left join Courses c on e.course_id = c.course_id;
+left join Courses c on e.course_id = c.course_id; 
 
 select s.student_id, s.student_name, c.course_id, c.course_name, e.enrolment_date from Students s
 left join Enrolments e on s.student_id = e.student_id
@@ -56,22 +56,29 @@ left join Students s on e.student_id = s.student_id;
 insert into Students(student_id,student_name,age) values
 (101,'Arshad',21),
 (102,'Rizwan',21),
-(103,'Thaju',21);
+(103,'Thaju',21),
+(104,'Faheem',22);
+
+insert into Students(student_id,student_name,age) values
+(105,'Fazal',22);
 
 insert into Courses(course_id,course_name,credits) values
 (201,'DBMS',3),
 (202,'Python Programming',3),
-(203,'Banking Technology',3);
+(203,'Banking Technology',3),
+(204,'Machine learning',3);
 
 insert into Instructors(instructor_id,instructor_name,department) values
 (301,'Angel','CS'),
 (302,'Tinku','CS'),
-(303,'Angel','Banking Technology');
+(303,'Angel','Banking Technology'),
+(304,'Dr.Raju','Machine Learning');
 
 insert into Enrolments(enrolment_id,student_id,course_id,instructor_id,enrolment_date) values
 (001,101,201,301,'2024-06-15'),
 (002,102,202,302,'2024-07-05'),
-(003,103,203,303,'2024-07-11');
+(003,103,203,303,'2024-07-11'),
+(004,104,204,304,'2024-08-25');
 
 
 
